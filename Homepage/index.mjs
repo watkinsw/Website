31a666498f3a6538bbfe5896cc12cc6cfe94fec0
Wesly <99352086+watkinsw@users.noreply.github.com>
2024-12-03 11:09:06 -0600

    Create index.mjs

---
+++ Homepage/index.mjs
@@ -0,0 +1,8 @@
+export const handler = async (event) => {
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('This is the homepage'),
+  };
+  return response;
+};

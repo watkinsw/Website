729778f4ddb77c87a696e25cf4488c31cd39519b
Wesly <99352086+watkinsw@users.noreply.github.com>
2024-12-16 20:34:36 -0600

    Update index.mjs

--- Homepage/index.mjs
+++ Homepage/index.mjs
@@ -1,8 +1,26 @@
+import { promises as fs } from 'fs';
+
 export const handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('This is the homepage'),
-  };
-  return response;
+  try {
+    const htmlContent = await fs.readFile('HomePage.html', 'utf-8');
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: htmlContent,
+    };
+    return response;
+    
+  } catch (error) {
+    console.error('Error reading the HTML file:', error);
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify('Internal Server Error'),
+    };
+
+    return response;
+  }
 };
